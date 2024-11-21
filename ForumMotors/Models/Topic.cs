abc42515f1d85a5588f6fe8ac45bba20ec3c48b9
vedran.krisztian <vedran.krisztian@BURG-1E05-13>
2024-11-21 09:16:23 +0100

    Add project files.

---
+++ ForumMotors/Models/Topic.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumMotors.Models
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Name {  get; set; }
+        public string Description { get; set; }
+        public string UserId { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ ForumMotors/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ForumMotors.Models
+{
+    public class User : IdentityUser
+    {
+        public string? FristName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
